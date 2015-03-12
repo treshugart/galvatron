9263c512f0068194b0e9a861f930f24fd35ac458
Trey Shugart <treshugart@gmail.com>
2015-03-12 13:49:35 +1100

    Don't assume any default options.

--- src/transform/babel.js
+++ src/transform/babel.js
@@ -2,13 +2,8 @@
 
 var babel = require('babel');
 
-module.exports = function () {
+module.exports = function (options) {
   return function (data) {
-    return babel.transform(data, {
-      // We have no idea what any dependencies might be up to. This
-      // ensures backward compatibility even though ES6 is strict.
-      blacklist: ['useStrict'],
-      modules: 'common'
-    }).code;
+    return babel.transform(data, options).code;
   };
 };
