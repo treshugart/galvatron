0b63e7c6864b3b49483033957aee364466cc23fe
Joshua Nelson <jonelson@atlassian.com>
2015-04-07 17:33:51 +1000

    Apply changes from branch define-replacement-only-if-needed, to 0.4

--- src/transform/globalize.js
+++ src/transform/globalize.js
@@ -33,13 +33,81 @@ function relative (file) {
   return path.relative(process.cwd(), file);
 }
 
+<<<<<<< Updated upstream
+=======
+function defineDependencies (imports) {
+  var code = '';
+  var keyVals = imports.map(function (imp) {
+    return '"' + imp.value + '": ' + generateModuleName(imp.path);
+  });
+
+  keyVals.unshift('"exports": exports');
+  keyVals.unshift('"module": module');
+  code = '\n' + indent(keyVals.join(',\n')) + '\n';
+  return '{' + code + '}';
+}
+
+function defineReplacement (name, deps, func) {
+  var rval;
+  var type;
+
+  func = [func, deps, name].filter(function (cur) { return typeof cur === 'function'; })[0];
+  deps = [deps, name, []].filter(Array.isArray)[0];
+
+  var allDependenciesDefined = deps.every(function (dep) {
+    return defineDependencies.hasOwnProperty(dep);
+  });
+
+  var hasExternalDependency = !allDependenciesDefined;
+  if (hasExternalDependency) {
+    originalDefine.apply(this, arguments);
+    return;
+  }
+
+  rval = func.apply(null, deps.map(function (value) { return defineDependencies[value]; }));
+  type = typeof rval;
+
+  // Some processors like Babel don't check to make sure that the module value
+  // is not a primitive before calling Object.defineProperty() on it. We ensure
+  // it is an instance so that it can.
+  if (type === 'string') {
+    rval = new String(rval);
+  } else if (type === 'number') {
+    rval = new Number(rval);
+  } else if (type === 'boolean') {
+    rval = new Boolean(rval);
+  }
+
+  // Reset the exports to the defined module. This is how we convert AMD to
+  // CommonJS and ensures both can either co-exist, or be used separately. We
+  // only set it if it is not defined because there is no object representation
+  // of undefined, thus calling Object.defineProperty() on it would fail.
+  if (rval !== undefined) {
+    exports = module.exports = rval;
+  }
+}
+
+>>>>>>> Stashed changes
 module.exports = function () {
   return function (data, info) {
     var sourcemap = data.match(regexSourcemap);
     sourcemap = sourcemap && sourcemap[1];
 
+<<<<<<< Updated upstream
     if (sourcemap) {
       data = data.replace(regexSourcemap, '');
+=======
+    // We assume CommonJS because that's what we're using to convert it.
+    shims.push('var module = {\n' + indent('exports: {}') + '\n};');
+    shims.push('var exports = module.exports;');
+
+    // We only need to generate the AMD -> CommonJS shim if it's used.
+    if (isAmd) {
+      shims.push('var originalDefine = window.define;');
+      shims.push('var defineDependencies = ' + defineDependencies(info.imports) + ';');
+      shims.push('var define = ' + defineReplacement + ';');
+      shims.push('define.amd = true;');
+>>>>>>> Stashed changes
     }
 
     info.imports.forEach(function (imp, index) {
