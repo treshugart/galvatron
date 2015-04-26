25d8142495554707bc3edd545bcad21a21ac700a
Will Binns-Smith <wbinnssmith@atlassian.com>
2015-04-26 01:03:33 -0700

    Use variations of node-detective to match imports

--- src/matcher/es6.js
+++ src/matcher/es6.js
@@ -1,13 +1,16 @@
 'use strict';
 
-var regexToArray = require('../util/regex-to-array');
+var detective = require('detective-es6');
+var debug = require('debug');
+var log = debug('galvatron:matcher:es6')
 
 module.exports = function ($fs) {
   return function (file, code) {
-    return regexToArray(/^\s*import[^'"]*['"]([^'"]+)['"];?/gm, code).map(function (imp) {
+    return detective(code).map(function (imp) {
+      log('resolving es6 dependency', imp, 'from', file);
       return {
-        path: $fs.ext($fs.resolve(imp[1], file), 'js', ['js', 'json']),
-        value: imp[1]
+        path: $fs.ext($fs.resolve(imp, file), 'js', ['js', 'json']),
+        value: imp
       };
     });
   };
