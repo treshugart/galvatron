5c8b6f2d3770b93ef1a02b36c97890686cbd875b
Trey Shugart <treshugart@gmail.com>
2015-04-27 13:35:33 +1000

    Ensure require()'s within comments aren't parsed.

--- src/matcher/commonjs.js
+++ src/matcher/commonjs.js
@@ -4,6 +4,8 @@ var regexToArray = require('../util/regex-to-array');
 
 module.exports = function ($fs) {
   return function (file, code) {
+    // Replace comments just in case they have require blocks in them.
+    code = code.replace(/(\/\*([\s\S]*?)\*\/)|(\/\/(.*)$)/gm, '');
     return regexToArray(/require\([\'"]([^\'"]+)[\'"]\)/g, code).map(function (imp) {
       return {
         path: $fs.ext($fs.resolve(imp[1], file), 'js', ['js', 'json']),
