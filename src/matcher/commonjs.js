3544b1633e5b435ab32fe4f892fb99fac5a589ea
Trey Shugart <treshugart@gmail.com>
2015-07-28 16:26:01 +1000

    Use detective-cjs instead of regexps.

--- src/matcher/commonjs.js
+++ src/matcher/commonjs.js
@@ -1,19 +1,15 @@
 'use strict';
 
-var debug = require('debug');
-var log = debug('galvatron:matcher:commonjs');
-var regexToArray = require('../util/regex-to-array');
-var removeComments = require('../util/remove-comments');
+var detective = require('detective-cjs');
+var log = require('debug')('galvatron:matcher:commonjs');
 
 module.exports = function ($fs) {
   return function (file, code) {
-    // Replace comments just in case they have require blocks in them.
-    code = removeComments(code);
-    return regexToArray(/require\([\'"]([^\'"]+)[\'"]\)/g, code).map(function (imp) {
+    return detective(code).map(function (imp) {
       log('resolving commonjs dependency', imp, 'from', file);
       return {
-        path: $fs.ext($fs.resolve(imp[1], file), 'js', ['js', 'json']),
-        value: imp[1]
+        path: $fs.ext($fs.resolve(imp, file), 'js', ['js', 'json']),
+        value: imp
       };
     });
   };
--- test/matcher.commonjs.js
+++ test/matcher.commonjs.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var expect = require('chai').expect;
 var mocha = require('mocha');
 var galv = require('../index')();
 
@@ -7,5 +8,12 @@ mocha.describe('matcher/commonjs', function () {
   mocha.it('should not error if containing ES6 syntax', function () {
     var data = 'import something from "./something";';
     galv.matcher('test.js', data);
+  });
+
+  mocha.it('returns dependencies', function () {
+    var data = 'require("something1"); require("something2")';
+    var deps = galv.matcher('test.js', data);
+    expect(deps[0].value).to.equal('something1');
+    expect(deps[1].value).to.equal('something2');
   });
 });
