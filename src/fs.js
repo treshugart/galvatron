db8da15bd350ba317520431e9c0e3aafac45d316
Trey Shugart <treshugart@gmail.com>
2015-08-24 21:53:12 +1000

    Resolve module/directory/index.js files

--- src/fs.js
+++ src/fs.js
@@ -36,12 +36,6 @@ Fs.prototype = {
   },
 
   module: function (request, relativeTo) {
-    if (request[0] === '.') {
-      // if the request is ./ or ../ etc, this isn't
-      // a request for a module
-      return;
-    }
-
     var mod;
     if (request.indexOf(path.sep) !== -1) {
       // the request is a deep lookup like `require('foo/bar/baz')`
@@ -59,36 +53,49 @@ Fs.prototype = {
     var dirs = relativeTo.split(path.sep);
     var foundFile;
 
-    check:
+    dirs:
     for (var a = 0; a < dirs.length; a++) {
+      lookups:
       for (var b in this.lookups) {
-        if (this.lookups.hasOwnProperty(b)) {
-          var checkDir = path.join(relativeTo, new Array(a).join('../'), b, mod);
-          var packageFile = path.join(checkDir, this.lookups[b]);
+        if (!this.lookups.hasOwnProperty(b)) {
+          continue lookups;
+        }
 
-          if (request.length && fs.existsSync(checkDir)) {
-            // this is a deep lookup into the package,
-            // resolve with that if the directory exists
-            foundFile = path.resolve(checkDir, request);
-            break check;
-          } else if (fs.existsSync(packageFile)) {
-            var pkg = require(packageFile);
+        var basePath = path.join(relativeTo, new Array(a).join('../'));
+        var modulePath = path.join(basePath, b, mod);
+        var moduleFile = path.join(modulePath, request);
+        var indexFile = path.join(basePath, 'index.js');
+        var packageFile = path.join(modulePath, this.lookups[b]);
 
-            if (pkg.main) {
-              // otherwise, return the module's main entry point.
-              var pkgMain = path.join(checkDir, pkg.main);
+        if (request && fs.existsSync(moduleFile)) {
+          // this is a deep lookup into the package,
+          // resolve with that if the directory exists
+          foundFile = moduleFile
+          break dirs;
+        }
 
-              if (fs.existsSync(pkgMain)) {
-                foundFile = pkgMain;
-                break check;
-              }
+        if (fs.existsSync(indexFile)) {
+          foundFile = indexFile;
+          break dirs;
+        }
 
-              pkgMain = pkgMain + '.js';
+        if (fs.existsSync(packageFile)) {
+          var pkg = require(packageFile);
 
-              if (fs.existsSync(pkgMain)) {
-                foundFile = pkgMain;
-                break check;
-              }
+          if (pkg.main) {
+            // otherwise, return the module's main entry point.
+            var pkgMain = path.join(modulePath, pkg.main);
+
+            if (fs.existsSync(pkgMain)) {
+              foundFile = pkgMain;
+              break dirs;
+            }
+
+            pkgMain = pkgMain + '.js';
+
+            if (fs.existsSync(pkgMain)) {
+              foundFile = pkgMain;
+              break dirs;
             }
           }
         }
@@ -120,9 +127,11 @@ Fs.prototype = {
   resolve: function (file, relativeTo) {
     file = this.expand(file);
 
-    return (path.isAbsolute(file) && file) ||
-      this.module(file, relativeTo) ||
-      this.relative(file, relativeTo);
+    if (path.isAbsolute(file)) {
+      return file;
+    }
+
+    return this.module(file, relativeTo) || this.relative(file, relativeTo);
   },
 
   relative: function (file, relativeTo) {
