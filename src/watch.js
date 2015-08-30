d39b174334d6c290a4088c0ebf1bcc4b87068186
Trey Shugart <treshugart@gmail.com>
2015-08-30 10:47:52 +1000

    Return stream from watch().

--- src/watch.js
+++ src/watch.js
@@ -2,9 +2,10 @@ var cache = require('./cache');
 var gulp = require('gulp');
 
 module.exports = function (pattern, task) {
-  task();
+  var stream = task();
   gulp.watch(pattern, function (f) {
     cache.expire(f.path);
     task();
   });
+  return stream;
 };
