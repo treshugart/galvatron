e97d0d2a3de603d394b042d5defe1a32ab18909f
Trey Shugart <treshugart@gmail.com>
2015-10-29 13:30:04 +1100

    Remove watch.

--- index.js
+++ index.js
@@ -3,6 +3,5 @@
 module.exports = {
   cache: require('./src/cache'),
   globalize: require('./src/globalize'),
-  trace: require('./src/trace'),
-  watch: require('./src/watch')
+  trace: require('./src/trace')
 };
